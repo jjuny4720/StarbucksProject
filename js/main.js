d32d8072411d4cb8e2075f957bc248eb30e0d6a4
jjuny4720@naver.com <jjuny4720@naver.com>
2022-02-11 23:31:38 +0900

    add index

--- js/main.js
+++ js/main.js
@@ -14,3 +14,22 @@ searchInputEl.addEventListener('blur', function (){
   searchEl.classList.remove('focused');
   searchInputEl.setAttribute('placeholder', '');
 });
+
+const badgeEL = document.querySelector('header .badges');
+
+window.addEventListener('scroll', _.throttle(function () {
+  console.log(window.scrollY);
+  if (window.scrollY > 500) {
+    // 배지 숨기기
+    // gsap.to(요소, 지속시간, 옵션);
+    gsap.to(badgeEL, .6, {
+      opacity: 0,
+      display : 'none'
+    });
+  } else {
+    gsap.to(badgeEL, .6, {
+      opacity: 1,
+      display : 'block'
+    });
+  }
+}, 300));
