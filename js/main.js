bf8c547255cb158776cf58f4c7c7828342eb77fe
jjuny4720@naver.com <jjuny4720@naver.com>
2022-02-18 10:55:34 +0900

    add js, main header

--- js/main.js
+++ js/main.js
@@ -35,10 +35,23 @@ window.addEventListener('scroll', _.throttle(function () {
 }, 300));
 
 
-const fadeEls = document.querySelectorAll('.visual .fade-in');
+/**
+ * 순서대로 나타나는 기능
+ */
+// 나타날 요소들(.fade-in) 찾기.
+const fadeEls = document.querySelectorAll('.visual .fade-in')
+// 나타날 요소들을 하나씩 반복해서 처리!
 fadeEls.forEach(function (fadeEl, index) {
+  // 각 요소들을 순서대로(delay) 보여지게 함!
   gsap.to(fadeEl, 1, {
-    delay: (index + 1)*.7, //0.7, 1.4, 2.1, 2.7
+    delay: (index + 1) * .7,
     opacity: 1
-  });
-});
+  })
+})
+
+//new Swiper(선택자, 옵션)
+new Swiper('.notice-line .swiper-container', {
+  direction: 'vertical', // 수직 슬라이드
+  autoplay: true, // 자동 재생 여부
+  loop: true // 반복 재생 여부
+})
