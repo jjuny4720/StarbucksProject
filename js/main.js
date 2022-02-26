5bfacb2d9cee9cb7b8d4433dd54736ed581409a0
jjuny4720@naver.com <jjuny4720@naver.com>
2022-02-26 22:03:28 +0900

    add index

--- js/main.js
+++ js/main.js
@@ -74,7 +74,11 @@ new Swiper('.promotion .swiper-container', {
     prevEl: '.promotion .swiper-prev', // 이전 버튼 선택자
     nextEl: '.promotion .swiper-next' // 다음 버튼 선택자
   }
-})
+});
+
+
+
+
 new Swiper('.awards .swiper-container', {
   // direction: 'horizontal', // 수평 슬라이드
   autoplay: true, // 자동 재생 여부
@@ -86,7 +90,20 @@ new Swiper('.awards .swiper-container', {
     prevEl: '.awards .swiper-prev', // 이전 버튼 선택자
     nextEl: '.awards .swiper-next' // 다음 버튼 선택자
   }
-})
+});
+
+new Swiper('.awards .swiper-container', {
+  autoplay:true,
+  loop:true,
+  spaceBetween: 30,
+  slidesPerView: 5,
+  navigation: {
+    prevEl: '.awards .swiper-prev',
+    nextEl: '.awards .swiper-next',
+  }
+});
+
+
 
 const promotionEl = document.querySelector('.promotion');
 const promotionToggleBtn = document.querySelector('.toggle-promotion')
@@ -100,4 +117,40 @@ promotionToggleBtn.addEventListener('click', function(){
     //보임 처리!
     promotionEl.classList.remove('hide');
   }
-})
+});
+
+// 범위 랜덤 함수(소수점 2자리까지)
+function random(min, max) {
+  // `.toFixed()`를 통해 반환된 문자 데이터를,
+  // `parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 변환
+  return parseFloat((Math.random() * (max - min) + min).toFixed(2))
+};
+
+function floatingObject(selector, delay, size){
+  // gsap.to(요소, 시간, 옵션);
+  gsap.to(
+      selector,  //선택자
+      random(1.5, 2.5),  //애니메이션 동작 시간
+    { //옵션
+      y: size,
+      repeat: -1,
+      yoyo: true,
+      ease: Power1.easeInout,
+      delay: random(0, delay)
+    }
+  );
+}
+floatingObject('.floating1', 1, 15);
+floatingObject('.floating2', .5, 15);
+floatingObject('.floating3', 1.5, 20);
+
+const spyEls = document.querySelectorAll('section.scroll-spy');
+spyEls.forEach(function (spyEl) {
+  new ScrollMagic
+    .Scene({
+      triggerElement: spyEl,//보여짐 여부를 감시할 요소를 지정
+      triggerHook:.8
+    }) 
+    .setClassToggle(spyEl, 'show') 
+    .addTo(new ScrollMagic.Controller());
+});
