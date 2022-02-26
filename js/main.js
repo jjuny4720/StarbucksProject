50458dda837ce468144a19e54c1a03c7079b8440
jjuny4720@naver.com <jjuny4720@naver.com>
2022-02-26 22:54:06 +0900

    add index

--- js/main.js
+++ js/main.js
@@ -16,6 +16,7 @@ searchInputEl.addEventListener('blur', function (){
 });
 
 const badgeEL = document.querySelector('header .badges');
+const toTopEl = document.querySelector('#to-top');
 
 window.addEventListener('scroll', _.throttle(function () {
   console.log(window.scrollY);
@@ -26,13 +27,27 @@ window.addEventListener('scroll', _.throttle(function () {
       opacity: 0,
       display : 'none'
     });
+    // 버튼 보이기!
+    gsap.to(toTopEl, .2, {
+      x:0
+    });
   } else {
     gsap.to(badgeEL, .6, {
       opacity: 1,
       display : 'block'
     });
+    // 버튼 숨기기!
+    gsap.to(toTopEl, .2, {
+      x: 100
+    });
   }
 }, 300));
+
+toTopEl.addEventListener('click', function () {
+  gsap.to(window, .7, {
+    scrollTo: 0
+  });
+});
 
 
 /**
@@ -154,3 +169,7 @@ spyEls.forEach(function (spyEl) {
     .setClassToggle(spyEl, 'show') 
     .addTo(new ScrollMagic.Controller());
 });
+
+
+const thisYear = document.querySelector('.this-year');
+thisYear.textContent = new Date().getFullYear(); //2021
